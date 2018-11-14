dcdc0c2a88fab9938cd6cc05b36126b5ec39c51e
billbulanbaru <bill@bulan-baru.com>
2018-11-15 01:10:10 +0800

    Initial commit

---
+++ Utilities/Format.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Format
+    {
+        public static String AddSlashes(String s)
+        {
+            return s.Replace("'", "\\'");
+        }
+
+        public static String[] AddSlashes(String[] s)
+        {
+            if (s == null) return null;
+            String[] s2 = new string[s.Length];
+            for(int i = 0; i < s.Length; i++)
+            {
+                s2[i] = AddSlashes(s[i]);
+            }
+
+            return s2;
+        }
+    }
+}
---
+++ Utilities/SerialPorts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Utilities
+{
+    public static class SerialPorts
+    {
+        public static List<String> Find(String searchOn)
+        {
+            List<String> foundPorts = new List<String>();
+
+            //loop through available COM ports looking for searchOn
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+                var portList = portnames.Select(n => n + ": " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+                foreach (String p in portList)
+                {
+                    String[] parts = p.Split(':');
+                    if (parts.Length > 1 && parts[1].Contains(searchOn)) foundPorts.Add(parts[0]);
+                }
+            }
+
+            return foundPorts;
+        }
+    }
+}
