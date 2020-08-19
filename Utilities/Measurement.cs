18df357ceec740cbb91b751a248f26ac4a077191
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-19 13:01:08 +0800

    g

--- Utilities/Measurement.cs
+++ Utilities/Measurement.cs
@@ -9,6 +9,63 @@ namespace Chetch.Utilities
 {
     public static class Measurement
     {
+        public const double METER_2_CM = 100;
+        public const double MM_2_M = 1000;
+
+        public const double SPEED_OF_SOUND_MPS = 343.0;
+
+        public enum Unit
+        {
+            NONE,
+            CM,
+            METER, //standarad
+            MM,
+            KG, //standard
+            GRAM,
+            CELSIUS, //standard
+            SECOND, //standard
+            HOUR,
+            MINUTE,
+            MICROSECOND,
+            MILLISECOND
+        }
+
+        //returns number of standard units that are equal to the given unit
+        //e.g. 1 cm is given, returned is 0.01 (as in 0.01 meters)
+        static private double _asStandardUnit(Unit unit)
+        {
+            switch (unit)
+            {
+                case Unit.CM:
+                    return 0.01;
+
+                case Unit.MM:
+                case Unit.GRAM:
+                case Unit.MILLISECOND:
+                    return 0.001;
+                    
+                case Unit.MICROSECOND:
+                    return 0.000001;
+
+                case Unit.HOUR:
+                    return 3600.0;
+
+                case Unit.MINUTE:
+                    return 60.0;
+            }
+            return 1;
+        }
+
+        static public double ConvertUnit(double value, Unit from, Unit to = Unit.NONE)
+        {
+            return value * _asStandardUnit(from) / _asStandardUnit(to);
+        }
+
+        static public double ConvertUnit(Unit from, Unit to = Unit.NONE)
+        {
+            return ConvertUnit(1, from, to);
+        }
+
         static public double GetDistance(double lat1, double lng1, double lat2, double lng2, bool coordsInDegrees = true)
         {
             if (!coordsInDegrees) // conver to degrees first
