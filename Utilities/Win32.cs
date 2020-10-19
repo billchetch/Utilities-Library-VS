5fdf93b63076baceda5d9c711efb1455152b02de
billchetch <45006134+billchetch@users.noreply.github.com>
2020-10-19 15:10:04 +0800

    s

---
+++ Utilities/Win32.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Chetch.Utilities
+{
+    public static class Win32
+    {
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool IsWow64Process(IntPtr hProcess, out bool Wow64Process);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool Wow64DisableWow64FsRedirection(out IntPtr OldValue);
+
+        public static bool DisableWow64FsRedirection()
+        {
+            bool bWow64 = false;
+            IsWow64Process(System.Diagnostics.Process.GetCurrentProcess().Handle, out bWow64);
+            if (bWow64)
+            {
+                IntPtr OldValue = IntPtr.Zero;
+                bool bRet = Wow64DisableWow64FsRedirection(out OldValue);
+            }
+            return bWow64;
+        }
+    }
+}
