801b6964fd2015cd99d43fd5390b3a8fb3f01b6e
billchetch <45006134+billchetch@users.noreply.github.com>
2021-09-26 10:09:32 +0800

    s

--- Utilities/Format.cs
+++ Utilities/Format.cs
@@ -37,5 +37,32 @@ namespace Chetch.Utilities
             r = regex.Replace(r, " ");
             return r;
         }
+
+        public static Dictionary<String, String> ParsCommandLineArguments(String[] args)
+        {
+            Dictionary<String, String> startArgs = new Dictionary<String, String>();
+            String[] optionPrefixes = new string[] { "--", "/" };
+            String option = null;
+            for (int i = 0; i < args.Length; i++)
+            {
+                String s = args[i].Trim();
+                bool isOption = false;
+                foreach (var prefix in optionPrefixes)
+                {
+                    if (s.IndexOf(prefix) == 0)
+                    {
+                        option = s.Substring(prefix.Length).ToLower();
+                        startArgs[option] = null;
+                        isOption = true;
+                        break;
+                    }
+                }
+                if (!isOption && i > 0)
+                {
+                    startArgs[option] = s;
+                }
+            }
+            return startArgs;
+        }
     }
 }
