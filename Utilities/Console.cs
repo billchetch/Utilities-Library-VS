48ca999a872ab35ab90b353a5dfb943bcf85b2cf
billchetch <45006134+billchetch@users.noreply.github.com>
2022-01-21 20:19:08 +0800

    s

---
+++ Utilities/Console.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chetch.Utilities
+{
+    public static class Console
+    {
+        static public void PK(String text)
+        {
+            System.Console.WriteLine(text);
+            System.Console.ReadKey(true);
+        }
+    }
+}
